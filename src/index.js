a1132311214089e2259c28a3b8bb05add9d4e73d
euler5926 <euler5926@gmail.com>
2021-06-08 20:24:55 +1000

    fd

--- src/index.js
+++ src/index.js
@@ -167,7 +167,6 @@ class Home extends React.Component {
      }
   
   componentDidMount() {
-    this.display();
     $(document).ready(()=>{
       $("#orderform").on('click', '.btnSelect', function() {
         var currentRow = $(this).closest("tr");
