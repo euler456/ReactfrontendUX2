08a85a001aa6e25a5029cdba876258ea7cb3e355
euler5926 <euler5926@gmail.com>
2021-06-12 01:03:40 +1000

    fhgfdg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
 import Loader from "react-loader-spinner";
-import $, { extend } from 'jquery';
+
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Displayfood from '../src/displayfood'
 //"homepage": "http:euler456.github.io/UX2",
@@ -167,49 +167,7 @@ class Home extends React.Component {
     .catch(function(error) {console.log(error)});
      }
   
-     orderfood=()=>{
-       alert("order food");
-      $("#orderform").on('click', '.btnSelect', function() {
-        var currentRow = $(this).closest("tr");
-        var col1 = currentRow.find(".fd-value").val(); 
-        var col2 = currentRow.find(".fd-id").html(); 
-        var col3 = currentRow.find(".fd-name").html(); 
-        var col4 = currentRow.find(".price").html();
-        var col5 =col4 * col1;
-        if(col1 !=0){
-        var fd = new FormData();
-        fd.append('F_ID',col2 );
-        fd.append('foodname', col3 );
-        fd.append('price', col4 );
-        fd.append('quantity', col1 );
-        fd.append('totalprice', col5 );
-        fetch('https://ux2backend.herokuapp.com/api/api.php?action=orderquantity', 
-        {
-            method: 'POST',
-            body: fd,
-            credentials: 'include'
-        })
-       .then((headers)=> {
-            if(headers.status == 400) {
-                console.log('fail to add');
-                return;
-            }
-            if(headers.status == 201) {
-              alert("add successful");
-                console.log('addfood succussful');
-                localStorage.setItem('action','add food');  
-              this.display();
-                return;
-            }
-        })
-        .catch((error)=> {console.log(error)});}
-        else{
-            alert("please select value");
-        }
-      
-      });
-  
-     }
+    
   
   render(){
     const { hits } = this.state; 
