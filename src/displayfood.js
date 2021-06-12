ad33dc21448011d40694be9e92466c250d38d678
euler5926 <euler5926@gmail.com>
2021-06-12 20:25:45 +1000

    dsfd

--- src/displayfood.js
+++ src/displayfood.js
@@ -76,47 +76,48 @@ function Displayfood() {
 
   function orderfood(){
     alert("order food");
-   $("#orderform").on('click', '.btnSelect', function() {
-     var currentRow = $(this).closest("tr");
-     var col1 = currentRow.find(".fd-value").val(); 
-     var col2 = currentRow.find(".fd-id").html(); 
-     var col3 = currentRow.find(".fd-name").html(); 
-     var col4 = currentRow.find(".price").html();
-     var col5 =col4 * col1;
-     if(col1 !=0){
-     var fd = new FormData();
-     fd.append('F_ID',col2 );
-     fd.append('foodname', col3 );
-     fd.append('price', col4 );
-     fd.append('quantity', col1 );
-     fd.append('totalprice', col5 );
-     fetch('https://ux2backend.herokuapp.com/api/api.php?action=orderquantity', 
-     {
-         method: 'POST',
-         body: fd,
-         credentials: 'include'
-     })
-    .then((headers)=> {
-         if(headers.status == 400) {
-             console.log('fail to add');
-             return;
-         }
-         if(headers.status == 201) {
-           alert("add successful");
-             console.log('addfood succussful');
-             localStorage.setItem('action','add food');  
-          display();
-             return;
-         }
-     })
-     .catch((error)=> {console.log(error)});}
-     else{
-         alert("please select value");
-     }
    
-   });
   };
   useEffect(() => {
+    $("#orderform").on('click', '.btnSelect', function() {
+      var currentRow = $(this).closest("tr");
+      var col1 = currentRow.find(".fd-value").val(); 
+      var col2 = currentRow.find(".fd-id").html(); 
+      var col3 = currentRow.find(".fd-name").html(); 
+      var col4 = currentRow.find(".price").html();
+      var col5 =col4 * col1;
+      if(col1 !=0){
+      var fd = new FormData();
+      fd.append('F_ID',col2 );
+      fd.append('foodname', col3 );
+      fd.append('price', col4 );
+      fd.append('quantity', col1 );
+      fd.append('totalprice', col5 );
+      fetch('https://ux2backend.herokuapp.com/api/api.php?action=orderquantity', 
+      {
+          method: 'POST',
+          body: fd,
+          credentials: 'include'
+      })
+     .then((headers)=> {
+          if(headers.status == 400) {
+              console.log('fail to add');
+              return;
+          }
+          if(headers.status == 201) {
+            alert("add successful");
+              console.log('addfood succussful');
+              localStorage.setItem('action','add food');  
+           display();
+              return;
+          }
+      })
+      .catch((error)=> {console.log(error)});}
+      else{
+          alert("please select value");
+      }
+    
+    });
     display();
   fetch('https://ux2backend.herokuapp.com/api/api.php?action=displayorderfood',
   {
@@ -149,7 +150,7 @@ function Displayfood() {
     <td class='price'>{hit.price}</td>
     <td><input type="number" class="fd-value" name="quantity" min="0" max="50"></input></td>
     <td>{hit.options}</td>
-    <td><input type="submit" name="submit" class="btnSelect" onClick={() =>orderfood()}></input></td>
+    <td><input type="submit" name="submit" class="btnSelect" ></input></td>
      </tr>
           ) )}
     </tbody>
