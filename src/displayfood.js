23933aa039c4e86b85a12864aedeea851bf259c7
euler5926 <euler5926@gmail.com>
2021-06-12 14:49:42 +1000

    sad

--- src/displayfood.js
+++ src/displayfood.js
@@ -62,7 +62,7 @@ function Displayfood() {
         }
         if(headers.status == 201) {
             console.log('sumtotalprice');
-           this.setredirect({ redirect: true 
+        setredirect({ redirect: true 
               });
             localStorage.setItem('reload','has been reload');   
             localStorage.setItem('action','checking out');   
